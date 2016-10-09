c67b6446424a2fc511283b89f81f8fc8e364cf68
Peter <suilong24@yahoo.com>
2016-10-09 01:59:54 -0400

    Added working API and test case

--- api/application.py
+++ api/application.py
@@ -1,9 +1,8 @@
-from flask import Flask, request,jsonify
+from flask import Flask, request, jsonify
 import json
 import requests
 application = Flask(__name__)
 #"Content-Type": "application/json"
-parameters = { "key": "5b6061eec9a7163d416beb0923c8f830", "rId":""}
 
 @application.route('/')
 def index():
@@ -14,22 +13,47 @@ def index():
 def api_add1():
 	return str(int(request.args.get('num')) + 10)
 
-
+#
 @application.route('/harambe/extract_ingredient',methods =['GET'])
 def extract_ingredient():
-	receipe = " ".join(str(request.args.get('recipe')).split('_'))
+	recipe = " ".join(request.args.get('recipe').encode("utf-8").split('_'))
+	#print(recipe)
 	GET_URL = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/autocomplete"
 	header={
 	  "X-Mashape-Key": "5ZjOXA7FMamsh4fTmzaEwHnOh2a4p1T053Djsn3LzLw2PS0Kch",
 	  "Accept": "application/json"
 	}
-  	param ={"number": 5, "query": receipe}
-  	response = requests.get(GET_URL, headers=header, params=param)
+  	param ={"number": 5, "query": recipe}
+  	recipeList = requests.get(GET_URL, headers=header, params=param)
+  	#print(recipeList.json())
   	idSearch = {}
-  	for dicts in response.json():
-  		idSearch[dicts["title"]] = dicts["id"]
-  	return jsonify(idSearch)
+  	for dicts in recipeList.json():
+  		idSearch[dicts["title"].encode("utf-8")] = dicts["id"]
+  	#print(idSearch)
+  	return jsonify(recipe_ingredient(idSearch[idSearch.keys()[0]]))
 
+def recipe_ingredient(recipeId):
+	#print("*"*80)
+	#print(recipeId)
+	GET_URL = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/"+str(recipeId)+"/analyzedInstructions"
+	header={
+	    "X-Mashape-Key": "5ZjOXA7FMamsh4fTmzaEwHnOh2a4p1T053Djsn3LzLw2PS0Kch",
+	    "Accept": "application/json"
+ 	}
+ 	instructStep = requests.get(GET_URL, headers=header)
+ 	m = instructStep.json()
+ 	#print("="*100)
+ 	#print(m)
+ 	k = m[0]["steps"]
+ 	instructions = []
+ 	instructIngredients = []
+ 	for dicts in k:
+ 		instructions.append(dicts["step"].encode("utf-8"))
+ 		instructIngredients+=dicts["ingredients"]
+	ingredient = [dicts["name"].encode("utf-8") for dicts in instructIngredients]
+	#print("-"*80)
+	#print(ingredient)
+	return {"ingredients":" ".join(ingredient),"instructions": " ".join(instructions) }
 
 '''
 @application.route('/api/apiTesting', methods=['GET'])
@@ -40,20 +64,6 @@ def api_apiTesting():
 	#return("recipe ID"+ header["rId"])
 	response = requests.get(GET_URL, params=parameters)
 	ingredient = response.json().get("ingredients")
-'''
-#TO-DO: fix the method to POST base on dish
-#route :
-'''
-@application.route('/api/return_ingredient', methods=['GET'])
-def return_ingredient():
-	dish name = request.args.get('dish')
-	#request.args.get()
-'''
-#return shaved off string ingredient to front-end
-'''
-@application.route('/api/ingredient_to_shopping_cart', methods=['GET'])
-def ingredient_to_shopping_cart():
-	return(some shaved off list of ingredient)
 '''
 
 if __name__ == '__main__':
